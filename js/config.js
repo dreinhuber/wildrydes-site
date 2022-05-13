09b9ee2215c5f863834d87ce373c2b7861967f60
Daniel Reinhuber <dreinhuber@gmail.com>
2022-05-12 23:53:55 -0500

    added cognito configs

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_9HX7rkHez', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6ig0td3rlvvmc3vaoa77om167v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
